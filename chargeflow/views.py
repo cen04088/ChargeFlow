a7854f702c7a967b047ab72d827f2a9ea533d262
yyj <yyj07081@gmail.com>
2026-03-24 09:35:51 +0900

    indexchange

--- chargeflow/views.py
+++ chargeflow/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
